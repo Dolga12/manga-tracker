a4eb2df6e919d1cd5eb073db610acbd10ae3d2aa
Dolga12 <doriansmpro@gmail.com>
2025-03-31 10:59:49 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Chaîne de connexion MongoDB - utilisez une valeur directe pour éviter les problèmes de variables d'environnement
-const MONGO_URI=mongodb+srv://test:u%m5%Th6w%rFjruTFKfHp@cluster.mongodb.net/manga_tracker?retryWrites=true&w=majority
+const MONGO_URI=mongodb+srv://test:u%m5%Th6w%rFjruTFKfHp@mangatracker.nsnqe.mongodb.net/?retryWrites=true&w=majority&appName=MangaTracker
 
 // Connexion à MongoDB simplifiée sans dépendance à mongodb-memory-server
 const connectDB = async () => {
